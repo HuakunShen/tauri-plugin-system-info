d0223668f8184024987ab867b952e829ea8e12f5
Huakun Shen <huakun.shen@huakunshen.com>
2024-06-07 12:08:37 -0400

    Add all commands to build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,32 @@
-const COMMANDS: &[&str] = &["ping", "execute"];
+const COMMANDS: &[&str] = &[
+    "all_sys_info",
+    "total_memory",
+    "used_memory",
+    "total_swap",
+    "used_swap",
+    "memory_info",
+    "hostname",
+    "name",
+    "kernel_version",
+    "os_version",
+    "static_info",
+    "components",
+    "cpus",
+    "cpu_count",
+    "cpu_info",
+    "disks",
+    "networks",
+    "processes",
+    "refresh_all",
+    "refresh_memory",
+    "refresh_cpu",
+    "refresh_processes",
+    "batteries",
+];
 
 fn main() {
-  tauri_plugin::Builder::new(COMMANDS)
-    .android_path("android")
-    .ios_path("ios")
-    .build();
+    tauri_plugin::Builder::new(COMMANDS)
+        .android_path("android")
+        .ios_path("ios")
+        .build();
 }
